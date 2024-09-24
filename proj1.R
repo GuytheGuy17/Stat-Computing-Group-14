e7b9ac1221c174278e7fb69e2011f2071f9b7da6
Guy McClennan <guymcclennan@Murzis-MacBook-Air.local>
2024-09-24 14:51:22 +0100

    Fixed Q6

--- proj1.R
+++ proj1.R
@@ -21,3 +21,42 @@ punct_to_remove <- c(",", ".", ";", "!", ":", "?")
 for(punct in punct_to_remove) {
   a <- split_punct(a, punct)
 } 
+
+# Function generating the m (=1000) most commonly occurring words for a vector
+most_common_words <- function(vec, m = 1000) {
+  # Convert words to lowercase 
+  vec <- tolower(vec)
+  
+  # Find the vector of unique words
+  unique <- unique(vec)
+  
+  # Determine the index vector
+  index <- match(vec, unique)
+  
+  # Count the number of occurrences
+  occurrences <- tabulate(index)
+  
+  # Determine the threshold frequency required to be included in the top 1000 words
+  sorted_occurrences <- occurrences[order(occurrences, decreasing = TRUE)]
+  threshold_frequency <- sorted_occurrences[m]
+  
+  # Store the indices that beat the threshold frequency 
+  most_common_indices <- c()
+  for(ind in c(1:length(occurrences))){
+    if(occurrences[ind] >= threshold_frequency){
+      most_common_indices = append(most_common_indices, ind)
+    }
+  }
+  
+  # Store the words corresponding to these indices (top 1000 words)
+  b <- c()
+  
+  for(ind in most_common_indices){
+    b = append(b, unique[ind])
+  }
+  
+  return(b)
+}
+
+length(most_common_words(a))
+
