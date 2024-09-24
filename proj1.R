91d64d3df5dd6e11d906ac3736e34289b7d6e361
Louis Bennett <louis21bennett@gmail.com>
2024-09-24 13:28:39 +0100

    Add split_punct function and clean a

--- proj1.R
+++ proj1.R
@@ -3,5 +3,21 @@ a <- scan("https://www.gutenberg.org/files/4300/4300-0.txt", what="character", s
 a <- gsub("_(","", a, fixed=TRUE) ## remove "_("
 
 split_punct <- function(vec, char) {
-  vec
+  esc_char <- paste0('\\', char)
+  
+  ii <- grep(esc_char, vec)
+  
+  out <- vector(mode = 'character', length = length(ii) + length(vec))
+  
+  out[ii + 1:length(ii)] <- char
+  
+  out[-c(ii + 1:length(ii))] <- gsub(esc_char, '', vec)
+  
+  return(out)
 }
+
+punct_to_remove <- c(",", ".", ";", "!", ":", "?")
+
+for(punct in punct_to_remove) {
+  a <- split_punct(a, punct)
+} 
