98946195bb25e46e77c31dfbcd1297dd7ba28686
Louis Bennett <louis21bennett@gmail.com>
2024-10-01 14:14:20 +0100

    Change to scanning 4300 from local

--- proj1.R
+++ proj1.R
@@ -1,5 +1,5 @@
 # setwd("put/your/local/repo/location/here") ## comment out of submitted
-a <- scan("https://www.gutenberg.org/files/4300/4300-0.txt", what="character", skip=73, nlines=32858-73, fileEncoding="UTF-8")
+a <- scan("4300-0.txt", what="character", skip=73, nlines=32858-73, fileEncoding="UTF-8")
 a <- gsub("_(","", a, fixed=TRUE) ## remove "_("
 
 #' split_punct
