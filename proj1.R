6427c5378deb47a21e6be98bab7ee298d92e675c
Guy McClennan <guymcclennan@Murzis-MacBook-Air.local>
2024-10-02 17:29:08 +0100

    Some minor comment changes

--- proj1.R
+++ proj1.R
@@ -10,21 +10,21 @@ a <- gsub("_(","", a, fixed=TRUE) ## remove "_("
 #' @param vec The word vector to search for punctuation marks.
 #' @param char The punctuation marks that are being searched for in the word vector.
 #' @return A word vector with the punctuation marks of the original word vector
-#' marked as unique inputs afer the word they were originally in.
+#' marked as unique inputs after the word they were originally in.
 
 split_punct <- function(vec, char) {
-  # escape the character as grep interprets some punctuation as regex
+  # Escape the character as grep interprets some punctuation as regex
   esc_char <- paste0('\\', char)
   
   ii <- grep(esc_char, vec)
   
-  # initialise output vector
+  # Initialise output vector
   out <- vector(mode = 'character', length = length(ii) + length(vec))
   
-  # store the (unescaped) character in the right place
+  # Store the (un-escaped) character in the right place
   out[ii + 1:length(ii)] <- char
   
-  # add the rest of the tokens back in, with the character removed
+  # Add the rest of the tokens back in, with the character removed
   out[-c(ii + 1:length(ii))] <- gsub(esc_char, '', vec)
   
   return(out)
@@ -61,7 +61,7 @@ most_common_words <- function(vec, m = 1000) {
   threshold_frequency <- sorted_occurrences[m]
   
   # Print the threshold frequency
-  print(paste('The threshold found was: ', threshold_frequency))
+  print(paste('The threshold frequency found was: ', threshold_frequency))
   
   # Store the indices that beat the threshold frequency 
   most_common_indices <- which(occurrences >= threshold_frequency)
@@ -85,14 +85,14 @@ length(b)
 #' @return The matrix M as defined in the question sheet
 
 create_M <- function(full_text, words, mlag = 4) {
-  # tokenise the full text vector
+  # Tokenise the full text vector
   match_ind <- match(full_text, words)
   
-  # initialise the matrix M
+  # Initialise the matrix M
   n <- length(full_text)
   M <- matrix(NA, nrow = n - mlag, ncol = mlag + 1)
   
-  # loop over and insert each column of the matrix, shifting the text along by one each time
+  # Loop over and insert each column of the matrix, shifting the text along by one each time
   for(i in 1:(mlag + 1)) {
     M[, i] <- match_ind[1:(n - mlag) + i - 1]
   }
@@ -112,59 +112,59 @@ M <- create_M(tolower(a), b, 4)
 #' @return Simulated text from full_text using a mlag order Markov model
 
 simulate_text <- function(full_text, words, nw = 50, mlag = 4) {
-  # create the matrix M 
-  # the tolower and gsub ensure the text and words are in the same form and will be matched
+  # Create the matrix M 
+  # The 'tolower' and 'gsub' ensure the text and words are in the same form and will be matched
   token_matrix <- create_M(tolower(full_text), tolower(gsub(' ', '',words)), mlag)
   
-  # initialise output
+  # Initialise output
   output <- integer(nw)
   
-  # sample the first token randomly from all non NAs
+  # Sample the first token randomly from all non NAs
   first_column <- token_matrix[, 1]
   valid_tokens <- first_column[!is.na(first_column)]
   output[1] <- sample(valid_tokens, 1)
   
-  # loop up to nw
+  # Loop up to nw
   for (i in 2:nw) {
-    # possible that i <= j for the first couple of words (before we've generated the first mlag words)
+    # Possible that i <= j for the first couple of words (before we've generated the first mlag words)
     for (j in (mlag):1) if (i>j) { 
-      # relevant sequence of words
+      # Relevant sequence of words
       context <- output[(i - j):(i - 1)]
       
-      # initialise the test matrix to find matching sequences in M
+      # Initialise the test matrix to find matching sequences in M
       test <- matrix(NA, nrow = NROW(token_matrix), ncol = j + 1)
       test[, 1] <- TRUE
       
-      # loop over the word section
+      # Loop over the word section
       for(k in 1:j) {
-        # check if the last column is TRUE and if the current word also matches
+        # Check if the last column is TRUE and if the current word also matches
         test[, k + 1] <- !is.na(token_matrix[, k]) & token_matrix[, k] == context[k] & test[, k]
       }
       
-      # find rows which matched the whole sequence
+      # Find rows which matched the whole sequence
       matched_rows <- which(test[, j + 1])
       
-      # if no matched rows try lower order model
+      # If no matched rows try lower order model
       if (length(matched_rows) > 0) {
-        # get the next tokens
+        # Get the next tokens
         next_tokens <- token_matrix[matched_rows, j + 1]
         token_table <- table(next_tokens)
         
         token_table <- token_table[!is.na(names(token_table))]
         
-        # calculate the probabilities of each token
+        # Calculate the probabilities of each token
         probs <- as.vector(token_table) / sum(token_table)
         
-        # if there's only one match or all are NA try lower order model
+        # If there's only one match or all are NA try lower order model
         if (length(token_table) > 1) {
-          # sample the next word then break from the loop
+          # Sample the next word then break from the loop
           output[i] <- as.integer(sample(names(token_table), 1, prob = probs))
           break 
         }
       }
     }
     
-    # if the first order model failed, use the 0th order (sampling randomly from all words)
+    # If the first order model failed, use the 0th order (sampling randomly from all words)
     if (is.na(output[i]) | output[i] == 0) {
       output[i] <- sample(valid_tokens, 1)
     }
@@ -173,6 +173,7 @@ simulate_text <- function(full_text, words, nw = 50, mlag = 4) {
   return(words[output])
 }
 
+## Generating a 50 word sequence with order 4 from the given text
 generated_text_1 <- simulate_text(a, b, mlag = 4)
 cat(generated_text_1, sep=" ")
 
@@ -196,33 +197,33 @@ frequency_simulation <- function(words, weights, size = 50){
   return(paste(section, collapse = " "))
 }
 
-# convert a to lowercase
+# Convert a to lowercase
 low <- tolower(a)
 
-# calculate the weights of each word
+# Calculate the weights of each word
 freq <- tabulate(match(low, b))
 
 frequency_simulation(low, freq, size = 50)
 
-# find the modified b vector
-# loop over b
+# Find the modified b vector
+# Loop over b
 b_mod <- sapply(b, function(x) {
-  # find all the matches in the lowercase text and get the original occurrences of them
+  # Find all the matches in the lowercase text and get the original occurrences of them
   matches <- a[which(low == x)]
   
-  # sort this and find the most common occurrence
+  # Sort this and find the most common occurrence
   most_common <- sort(table(matches), decreasing = TRUE)[1]
   
   names(most_common)
 })
 
-# add whitespace in front of non punctuation
+# Add whitespace in front of non punctuation
 b_mod[!b_mod %in% punct_to_remove] <- paste0(' ', b_mod[!b_mod %in% punct_to_remove])
 
-# example of generating text with modified b
+# Example of generating text with modified b
 generated_text_2 <- simulate_text(a, b_mod, mlag = 4)
 
-# need to remove the leading whitespace from first letter
+# Need to remove the leading whitespace from first letter
 generated_text_2[1] <- gsub(' ', '', generated_text_2[1])
 
 cat(generated_text_2, sep = '')
