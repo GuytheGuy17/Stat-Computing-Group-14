2de96aa1e4be69a8bcc89005059fe1df4bb180f2
Louis Bennett <louis21bennett@gmail.com>
2024-10-08 16:18:52 +0100

    Add shell of deconv function

--- proj2.R
+++ proj2.R
@@ -1,3 +1,4 @@
+
 ## File for second project
 setwd("~/Stat-Computing-Group-14")
 
@@ -16,15 +17,25 @@ pearson_stat <- function(observed_data, simulated_data) {
   return(sum)
 }
 
-basic_algorithm <- function(data, num_rows = 150, meanlog = 3.152, sdlog = 0.451, max_duration = 80) {
+pearson_stat_vec <- function(observed_data, simulated_data) {
+  denom <- simulated_data
+  denom[simulated_data <= 1] <- 1
   
+  sum((observed_data - simulated_data) ^ 2 / denom)
+}
+
+calc_days_to_death_probs <- function(max_duration = 80, meanlog = 3.152, sdlog = 0.451) {
   # Set up infection-to-death distribution
   probabilities <- dlnorm(1:max_duration, meanlog, sdlog)
-  probabilities <- probabilities / sum(probabilities)  # Normalize
+  probabilities / sum(probabilities)  # Normalize
+}
+
+create_t0 <- function(days, deaths, max_duration = 80) {
+  probabilities <- calc_days_to_death_probs(max_duration)
   
   # Calculate total deaths and create death days vector
-  total_deaths <- sum(data$nhs)
-  death_days <- rep(data$julian, data$nhs)
+  total_deaths <- sum(deaths)
+  death_days <- rep(days, deaths)
   
   # Generate infection-to-death durations
   durations <- sample(1:max_duration, total_deaths, replace = TRUE, prob = probabilities)
@@ -32,14 +43,104 @@ basic_algorithm <- function(data, num_rows = 150, meanlog = 3.152, sdlog = 0.451
   # Calculate initial guesses for infection days
   t0 <- death_days - durations
   
-  return(list(
-    data = data,
-    probabilities = probabilities,
-    total_deaths = total_deaths,
-    death_days = death_days,
-    t0 = t0
-  ))
+  return(t0)
 }
 
-basic_algorithm(data = initial_data)
+deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL) {
+  if(is.null(t0)) {
+    t0 <- create_t0(t, deaths)
+  }
+  
+  # extend deaths to 1 to 350 days
+  ext_deaths <- numeric(length = 350)
+  ext_deaths[t] <- deaths
+  
+  # initial vectors/matrices to store history of P and infections
+  P_hist <- numeric(length = n.rep)
+  inft <- matrix(nrow = length(ext_deaths), ncol = n.rep)
+  
+  # initialise days_opt - move this inside the loop and do dependent on j later
+  days_opt <- c(-4, -2, -1, 1, 2, 4)
+  
+  # calculate the probs for drawing later
+  probs <- calc_days_to_death_probs()
+  
+  # loop 
+  for(j in 1:n.rep) {
+    days_to_death <- sample(1:80, length(t0), replace = TRUE, prob = probs)
+    
+    pred_deaths <- tabulate(t0 + days_to_death)
+    
+    # extend pred_deaths to 350 to match the length
+    pred_deaths <- c(pred_deaths, rep(0, length = 350 - length(pred_deaths)))
+    
+    # calculate the P
+    P <- pearson_stat_vec(pred_deaths, ext_deaths)
+    
+    # sample without replacement to find the random ordering
+    ordering <- sample(1:length(t0), length(t0), replace = FALSE)
+    
+    # sort both vectors in same ordering
+    t0_sorted <- t0[ordering]
+    days_to_death_sorted <- days_to_death[ordering]
+    
+    # sample random changes
+    rand_change <- sample(days_opt, length(t0), replace = TRUE)
+    
+    for(i in 1:length(t0)) {
+      # Add 2 new vectors to store the proposed changes
+      t0_proposed <- t0_sorted
+      pred_deaths_proposed <- pred_deaths
+      
+      # add random change
+      t0_proposed[i] <- t0_sorted[i] + rand_change[i]
+      
+      # recalcuate day of death and the previous day of death
+      proposed_death_day <- t0_proposed[i] + days_to_death_sorted[i]
+      old_death_day <- t0_sorted[i] + days_to_death_sorted[i]
+      
+      # inject these into the predicted deaths
+      pred_deaths_proposed[proposed_death_day] <- pred_deaths_proposed[proposed_death_day] + 1
+      pred_deaths_proposed[old_death_day] <- pred_deaths_proposed[old_death_day] - 1
+      
+      P_proposed <- pearson_stat_vec(pred_deaths_proposed, ext_deaths)
+      
+      # if P_proposed < P then update all our vectors (strictly less than - in case of equality don't update for efficiency)
+      if(P_proposed < P) {
+        pred_deaths <- pred_deaths_proposed
+        t0_sorted <- t0_proposed
+        P <- P_proposed
+      }
+    }
+    
+    # undo the sorting of t0 and store it
+    reordering <- numeric(length = length(ordering))
+    reordering[ordering] <- 1:length(ordering)
+    
+    t0 <- t0_sorted[reordering]
+    
+    # save current state of variables
+    P_hist[j] <- P
+    inft[, j] <- pred_deaths
+  }
+  
+  # produce the plot here
+  
+  list(
+    P = P_hist,
+    inft = inft,
+    t0 = t0
+  )
+}
 
+# just using n.rep = 50 as example
+run <- deconv(initial_data$julian, initial_data$nhs, n.rep = 50)
+
+## comparison of 2 pearson stat functions 
+t <- Sys.time()
+lapply(1:100000, \(x) pearson_stat_vec(pred_deaths, ext_deaths))
+print(Sys.time() - t)
+
+t <- Sys.time()
+lapply(1:100000, \(x) pearson_stat(pred_deaths, ext_deaths))
+print(Sys.time() - t)
