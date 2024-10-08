23812bed835a5ec41a7274f0c427ba00c5102165
alexg2002 <girbanalexandru14@gmail.com>
2024-10-08 14:33:15 +0100

    Added modified Pearson statistic metric

--- proj2.R
+++ proj2.R
@@ -4,6 +4,18 @@ setwd("~/Stat-Computing-Group-14")
 # Only using the first 150 rows of the data for this practical.
 initial_data <- read.table("engcov.txt", nrows = 150)
 
+# Define the modified Pearson statistic to use as a metric for the goodness of fit
+# observed_data, simulated_data represent the vectors we are computing the Pearson statistic for 
+pearson_stat <- function(observed_data, simulated_data) {
+  sum <- 0
+  
+  for (i in 1:length(observed_data)){
+    sum <- sum + ((observed_data[i] - simulated_data[i]) ^ 2) / (max(1, simulated_data[i]))
+  }
+  
+  return(sum)
+}
+
 basic_algorithm <- function(data, num_rows = 150, meanlog = 3.152, sdlog = 0.451, max_duration = 80) {
   
   # Set up infection-to-death distribution
